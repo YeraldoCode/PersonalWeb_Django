d657da192b9ecddf6e47302bfc04c35858523d46
Yeraldo Avila <yeramtza@gmail.com>
2025-02-14 02:25:41 -0600

    Add new Django dependencies and fix environment variable access

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -20,7 +20,7 @@ import os
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environment.get('SECRET_KEY', default='skcsdcdsc')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='skcsdcdsc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
